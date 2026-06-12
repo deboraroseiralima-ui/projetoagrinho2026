9cdee9574412adcfbde5dae3e1e4c49702afff8c
deboraroseiralima-ui <debora.roseira.lima@escola.pr.gov.br>
2026-06-12 18:06:48 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+console.log('SustentAgro carregado!');
+
+document.querySelectorAll('.card').forEach(card=>{
+ card.addEventListener('click',()=>{
+  alert(card.querySelector('h3').innerText);
+ });
+});
